866308e39c3b21ddb30d1b66d5a78ad723495bb6
Tapio Vaattanen <tapio.vaattanen@nordcloud.com>
2024-08-04 14:31:22 +0300

    Initial commit – Tested with 13 000 photos.

---
+++ collect_gallery_meta_data.py
@@ -0,0 +1,238 @@
+#!/usr/bin/env python
+
+import requests
+from sqlalchemy import create_engine, Column, Integer, String, Text, ForeignKey, DateTime
+from sqlalchemy.orm import sessionmaker, relationship, declarative_base
+from urllib.parse import quote
+import logging
+import phpserialize
+import re
+import sys, os
+from datetime import datetime
+
+# Constants
+GALLERY_BASE_URL = os.getenv('GALLERY_BASE_URL', None)
+if GALLERY_BASE_URL is None:
+    raise ValueError("GALLERY_BASE_URL environment variable not set")
+
+Base = declarative_base()
+engine = create_engine(DATABASE_URL)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# Set up logging
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+class Album(Base):
+    __tablename__ = 'albums'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(255), nullable=False)
+    parent_id = Column(Integer, ForeignKey('albums.id'), nullable=True)
+    meta = Column(Text, nullable=True)
+    title = Column(String(255), nullable=True)
+    caption = Column(Text, nullable=True)
+    description = Column(Text, nullable=True)
+    subalbums = relationship('Album', backref='parent', remote_side=[id])
+    photos = relationship('Photo', backref='album')
+
+class Photo(Base):
+    __tablename__ = 'photos'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    album_id = Column(Integer, ForeignKey('albums.id'))
+    filename = Column(String(255), nullable=False)
+    capturedate = Column(DateTime, default=datetime.utcnow)
+    uploaddate = Column(DateTime, default=datetime.utcnow)
+    caption = Column(Text, nullable=True)
+    description = Column(Text, nullable=True)
+    url = Column(String(255), nullable=True)
+    meta = Column(Text, nullable=True)
+
+def create_tables():
+    Base.metadata.create_all(engine)
+
+def decode_chars(text):
+    if text is None:
+        return ''
+    char_map = {
+        '\\xC4': 'Ä',
+        '\\xE4': 'ä',
+        '\\xD6': 'Ö',
+        '\\xF6': 'ö',
+        '\\xC5': 'Å',
+        '\\xE5': 'å',
+        '\udcc4': 'Ä',
+        '\udce4': 'ä',
+        '\udcd6': 'Ö',
+        '\udcf6': 'ö',
+        '\udcc5': 'Å',
+        '\udce5': 'å'
+    }
+    pattern = re.compile('|'.join(re.escape(key) for key in char_map.keys()))
+    decoded_text = pattern.sub(lambda x: char_map[x.group()], text)
+    return decoded_text
+
+def fetch_data(url):
+    logger.debug(f"Fetching data from URL: {url}")
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.content
+
+def object_hook(name, obj):
+    return {key: value for key, value in obj.items()}
+
+def parse_album_data(data):
+    try:
+        album_data = phpserialize.loads(data, decode_strings=True, object_hook=object_hook)
+        logger.debug(f"Parsed album data: {album_data}")
+        fields = album_data.get('fields', {})
+        title = decode_chars(fields.get('title', ''))
+        description = decode_chars(fields.get('description', ''))
+        caption = decode_chars(fields.get('kuvausta', ''))
+        return title, caption, description
+    except Exception as e:
+        logger.error(f"Error deserializing album data: {e}")
+        return None, None, None
+
+def parse_photos_data(data):
+    def object_hook(name, obj):
+        return {key: value for key, value in obj.items()}
+
+    items = []
+    try:
+        data_dict = phpserialize.loads(data, decode_strings=True, object_hook=object_hook)
+        logger.debug(f"Parsed data_dict: {data_dict}")
+        for idx, item in data_dict.items():
+            try:
+                logger.debug(f"Processing item {idx}: {item}")
+                if item['image'] is None:  # This is an album
+                    album_dat_url = f"{GALLERY_BASE_URL}/{item['isAlbumName']}/album.dat"
+                    album_data = fetch_data(album_dat_url)
+                    title, caption, description = parse_album_data(album_data)
+                    logger.debug(f"Album data: title: {title}, caption: {caption}, description: {description}")
+                    album_name = item['isAlbumName']
+                    logger.debug(f"Detected sub-album with name: {album_name},\
+                                 caption: {caption}, title: {title}, description: {description}")
+                    items.append({'name': album_name, 'is_album': True,\
+                                  'caption': caption, 'title': title, 'description': description})
+                else:  # This is a photo
+                    caption_raw = item.get('caption', '')
+                    description_raw = item['extraFields'].get('Description', '') if 'extraFields' in item else ''
+                    caption = decode_chars(caption_raw)
+                    description = decode_chars(description_raw)
+                    raw_uploaddate = item.get('uploadDate', 0)
+                    uploaddate = datetime.utcfromtimestamp(raw_uploaddate).strftime('%Y-%m-%d %H:%M:%S')
+                    raw_capturedate = item.get('itemCaptureDate', datetime.utcnow())
+                    capturedt = datetime(int(raw_capturedate['year']),
+                                         int(raw_capturedate['mon']),
+                                         int(raw_capturedate['mday']),
+                                         int(raw_capturedate['hours']),
+                                         int(raw_capturedate['minutes']),
+                                         int(raw_capturedate['seconds']))
+                    capturedate = capturedt.strftime('%Y-%m-%d %H:%M:%S')
+                    url = GALLERY_BASE_URL + '/' + item['image']['name'] + '.' + item['image']['type']
+                    logger.debug(f"itemCaptureDate: {capturedate}, uploadDate: {uploaddate}")
+                    logger.debug(f"Raw caption: {caption_raw}, Raw description: {description_raw}")
+                    logger.debug(f"Decoded caption: {caption}, Decoded description: {description}")
+                    items.append({
+                        'name': item['image']['name'] + '.' + item['image']['type'],
+                        'is_album': False,
+                        'capturedate': capturedate,
+                        'uploaddate': uploaddate,
+                        'caption': caption,
+                        'description': description,
+                        'url': url
+                    })
+                    logger.debug(f"capturedate: {capturedate} uploaddate: {uploaddate} caption: {caption} description: {description} url: {url}")
+            except Exception as e:
+                logger.error(f"Error processing item: {item}")
+                logger.error(f"Exception: {e}")
+    except ValueError as e:
+        logger.error(f"Error deserializing data: {e}")
+        logger.error(f"Data: {data[:1000]}...")  # Log the first 1000 characters of the data for debugging
+
+    return items
+
+def insert_album(name, parent_id, meta, title, caption, description):
+    album = Album(name=name, parent_id=parent_id, meta=meta, title=title, caption=caption, description=description)
+    session.add(album)
+    try:
+        session.commit()
+        logger.info(f"Inserted album: {name} with parent_id: {parent_id}")
+        return album.id
+    except Exception as e:
+        session.rollback()
+        logger.error(f"Error inserting album: {e}")
+
+def insert_photo(album_id, filename, caption, description, url, meta, capturedate, uploaddate):
+    caption = caption.encode('utf-8', errors='ignore').decode('utf-8', errors='ignore')
+    description = description.encode('utf-8', errors='ignore').decode('utf-8', errors='ignore')
+    photo = Photo(album_id=album_id, filename=filename, caption=caption,
+                  description=description, url=url, meta=meta, capturedate=capturedate, uploaddate=uploaddate)
+    session.add(photo)
+    try:
+        session.commit()
+        logger.info(f"Inserted photo: {filename} in album_id: {album_id}")
+    except Exception as e:
+        session.rollback()
+        logger.error(f"Error inserting photo: {e}")
+
+def process_subalbum(album_name, parent_id):
+    encoded_album_name = quote(album_name)
+    photos_url = f"{GALLERY_BASE_URL}/{encoded_album_name}/photos.dat"
+    try:
+        photos_data = fetch_data(photos_url)
+        photos = parse_photos_data(photos_data)
+        for photo in photos:
+            if not photo['is_album']:
+                insert_photo(parent_id, photo['name'], photo['caption'],
+                             photo['description'], photo['url'],
+                             str(photo), photo['capturedate'], photo['uploaddate'])
+    except requests.HTTPError as e:
+        logger.warning(f"No photos.dat found for album {album_name}: {e}")
+    except Exception as e:
+        logger.error(f"Unexpected error processing album {album_name}: {e}")
+
+def process_root_album(root_album):
+    encoded_root_album = quote(str(root_album))
+    root_album_photos_url = f"{GALLERY_BASE_URL}/{encoded_root_album}/photos.dat"
+    root_album_dat_url = f"{GALLERY_BASE_URL}/{encoded_root_album}/album.dat"
+    try:
+        root_album_data = fetch_data(root_album_dat_url)
+        title, caption, description = parse_album_data(root_album_data)
+        root_album_id = insert_album(str(root_album), None, str(root_album),
+                                     title=title, caption=caption,
+                                     description=description)
+        root_album_photos_data = fetch_data(root_album_photos_url)
+        root_album_photos = parse_photos_data(root_album_photos_data)
+
+        for photo in root_album_photos:
+            if photo['is_album']:
+                sub_album_name = photo['name']
+                title = photo['title']
+                caption = photo['caption']
+                description = photo['description']
+                sub_album_id = insert_album(sub_album_name, root_album_id, sub_album_name, title, caption, description)
+                process_subalbum(sub_album_name, sub_album_id)
+            else:
+                insert_photo(root_album_id, photo['name'], photo['caption'], photo['description'], photo['url'], str(photo), photo['capturedate'], photo['uploaddate'])
+    except requests.HTTPError as e:
+        logger.warning(f"No photos.dat found for root_album {root_album}: {e}")
+    except Exception as e:
+        logger.error(f"Unexpected error processing root_album {root_album}: {e}")
+
+def main():
+    create_tables()
+
+    if len(sys.argv) < 2:
+        logger.error("Year not provided. Usage: ./get_album_data.py [YEAR]")
+        sys.exit(1)
+
+    root_album = sys.argv[1]
+
+    process_root_album(root_album)
+    session.close()
+
+if __name__ == "__main__":
+    main()
+
---
+++ execute_migration.py
@@ -0,0 +1,296 @@
+#!/usr/bin/env python
+
+import os, sys
+import requests
+import logging
+import mysql.connector
+from urllib.parse import quote
+from xml.etree import ElementTree
+import re
+
+# Constants
+GALLERY_BASE_URL =  os.getenv('GALLERY_BASE_URL', None)
+PIWIGO_API_URL = os.getenv('PIWIGO_API_URL', None) # 'http://[piwigo_host]/piwigo/ws.php'
+PIWIGO_USERNAME = os.getenv('PIWIGO_USERNAME', None)
+PIWIGO_PASSWORD = os.getenv('PIWIGO_PASSWORD', None)
+DOWNLOAD_DIR = "migration"
+
+DB_CONFIG = {
+    'host': os.getenv('MIG_DB_HOST', None),
+    'user': os.getenv('MIG_DB_USER', None),
+    'password': os.getenv('MIG_DB_PASSWORD', None),
+    'database': os.getenv('MIG_DB_NAME', None)
+}
+
+PW_DB_CONFIG = {
+    'host': os.getenv('PW_DB_HOST', None),
+    'user': os.getenv('PW_DB_USER', None),
+    'password': os.getenv('PW_DB_PASSWORD', None),
+    'database': os.getenv('PW_DB_NAME', None)
+}
+
+
+# Set up logging
+LOG_MSG_FORMAT = ('%(asctime)s,%(msecs)03d %(levelname)s [%(filename)s:%(lineno)d] - %(message)s')
+LOG_DATE_FORMAT = '%Y-%m-%dT%H:%M:%S'
+logFormatter = logging.Formatter(LOG_MSG_FORMAT, LOG_DATE_FORMAT)
+logging.basicConfig(level=logging.DEBUG, format=LOG_MSG_FORMAT, datefmt=LOG_DATE_FORMAT)
+logger = logging.getLogger(__name__)
+
+session = requests.Session()
+
+def decode_chars(text):
+    if text is None:
+        return ''
+    char_map = {
+        '\\xC4': 'Ä',
+        '\\xE4': 'ä',
+        '\\xD6': 'Ö',
+        '\\xF6': 'ö',
+        '\\xC5': 'Å',
+        '\\xE5': 'å',
+        '\udcc4': 'Ä',
+        '\udce4': 'ä',
+        '\udcd6': 'Ö',
+        '\udcf6': 'ö',
+        '\udcc5': 'Å',
+        '\udce5': 'å'
+    }
+    pattern = re.compile('|'.join(re.escape(key) for key in char_map.keys()))
+    decoded_text = pattern.sub(lambda x: char_map[x.group()], text)
+    return decoded_text
+
+def fetch_data(url):
+    logger.debug(f"Fetching data from URL: {url}")
+    response = requests.get(url)
+    response.raise_for_status()
+    return response.content  # Return raw bytes
+
+def piwigo_login():
+    data = {
+        'method': 'pwg.session.login',
+        'username': PIWIGO_USERNAME,
+        'password': PIWIGO_PASSWORD,
+    }
+    response = session.post(PIWIGO_API_URL, data=data)
+    logger.debug(f"Login response status code: {response.status_code}")
+    logger.debug(f"Login response text: {response.text}")
+    result = ElementTree.fromstring(response.content)
+    if result.get('stat') == 'ok':
+        logger.debug("Logged in to Piwigo")
+    else:
+        raise Exception("Failed to log in to Piwigo")
+
+def piwigo_create_album(name, parent_id, title, description):
+    data = {
+        'method': 'pwg.categories.add',
+        'name': title,
+        'parent': parent_id,
+        'comment': description,
+    }
+    response = session.post(PIWIGO_API_URL, data=data)
+    logger.debug(f"Create album response status code: {response.status_code}")
+    logger.debug(f"Create album response text: {response.text}")
+    result = ElementTree.fromstring(response.content)
+    if result.get('stat') == 'ok':
+        album_id = result.find('id').text
+        logger.debug(f"Created album {title} with ID {album_id}")
+        return album_id
+    else:
+        raise Exception(f"Failed to create album {title}")
+
+def download_image(url, filepath):
+    logger.debug(f"Downloading image from URL: {url} to filepath: {filepath}")
+    response = requests.get(url)
+    response.raise_for_status()
+    with open(filepath, 'wb') as f:
+        f.write(response.content)
+    logger.debug(f"Downloaded image to {filepath}")
+
+def update_album_info(album_id, title, caption, description):
+    data = {
+        'method': 'pwg.categories.setInfo',
+        'category_id': album_id,
+        'name': title,
+        'comment': f"{caption} - {description}" if caption else description
+    }
+    response = session.post(PIWIGO_API_URL, data=data)
+    logger.debug(f"Update album info response status code: {response.status_code}")
+    logger.debug(f"Update album info response text: {response.text}")
+    result = ElementTree.fromstring(response.content)
+    if result.get('stat') != 'ok':
+        raise Exception(f"Failed to update album info for album ID {album_id}")
+
+def process_album(album_name):
+    conn = mysql.connector.connect(**DB_CONFIG)
+    cursor = conn.cursor(dictionary=True)
+    pw_conn = mysql.connector.connect(**PW_DB_CONFIG)
+    pw_cursor = pw_conn.cursor(dictionary=True)
+
+    # Fetch album info from database
+    cursor.execute("SELECT * FROM albums WHERE name=%s", (album_name,))
+    album = cursor.fetchone()
+    logger.debug(f"Album fetched: {album}")
+    if not album:
+        logger.error(f"Album {album_name} not found in database")
+        return
+
+    album_id = album['id']
+    album_title = album['title']
+    album_caption = album['caption']
+    album_description = album['description']
+
+    # Check if the album is already created
+    if album['created']:
+        logger.info(f"Album {album_name} already created, skipping album creation")
+        pw_cursor.execute("SELECT id FROM piwigo_categories WHERE name=%s", (album_title,))
+        result = pw_cursor.fetchone()
+        if result:
+            logger.debug(f"Found album {album_title} in Piwigo database with ID {result['id']}")
+            piwigo_album_id = result['id']
+        else:
+            logger.error(f"Could not find album {album_title} in Piwigo database")
+            return
+        # Process sub-albums
+        cursor.execute("SELECT * FROM albums WHERE parent_id=%s", (album_id,))
+        subalbums = cursor.fetchall()
+        logger.debug(f"Subalbums fetched: {subalbums}")
+        for subalbum in subalbums:
+            subalbum_id = subalbum['id']
+            subalbum_name = subalbum['name']
+            subalbum_title = subalbum['title']
+            subalbum_caption = subalbum['caption']
+            subalbum_description = subalbum['description']
+
+            if subalbum['created']:
+                logger.info(f"Album {subalbum_name} already created, skipping album creation")
+                pw_cursor.execute("SELECT id FROM piwigo_categories WHERE name=%s", (subalbum_title,))
+                result = pw_cursor.fetchone()
+                if result:
+                    logger.debug(f"Found album {subalbum_title} in Piwigo database with ID {result['id']}")
+                    piwigo_subalbum_id = result['id']
+                else:
+                    logger.error(f"Could not find sub album {subalbum_title} in Piwigo database")
+                    return
+            else:
+                # Process sub-albums
+                piwigo_login()
+                piwigo_subalbum_id = piwigo_create_album(subalbum_name, piwigo_album_id, subalbum_title, subalbum_description)
+                cursor.execute("UPDATE albums SET created=TRUE WHERE id=%s", (subalbum_id,))
+                conn.commit()
+            process_photos(subalbum_id, subalbum_name, piwigo_subalbum_id)
+    else:
+        # Login to Piwigo
+        piwigo_login()
+
+        # Create album in Piwigo
+        logger.debug(f"Creating album {album_title} in Piwigo")
+        piwigo_album_id = piwigo_create_album(album_name, None, album_title, album_description)
+
+        # Update album info in database
+        cursor.execute("UPDATE albums SET created=TRUE WHERE id=%s", (album_id,))
+        conn.commit()
+
+        # Process sub-albums
+        cursor.execute("SELECT * FROM albums WHERE parent_id=%s", (album_id,))
+        subalbums = cursor.fetchall()
+        logger.debug(f"Subalbums fetched: {subalbums}")
+        for subalbum in subalbums:
+            subalbum_id = subalbum['id']
+            subalbum_name = subalbum['name']
+            subalbum_title = subalbum['title']
+            subalbum_caption = subalbum['caption']
+            subalbum_description = subalbum['description']
+
+            piwigo_subalbum_id = piwigo_create_album(subalbum_name, piwigo_album_id, subalbum_title, subalbum_description)
+
+            cursor.execute("UPDATE albums SET created=TRUE WHERE id=%s", (subalbum_id,))
+            conn.commit()
+
+            process_photos(subalbum_id, subalbum_name, piwigo_subalbum_id)
+
+        # Process photos in the main album
+        process_photos(album_id, album_name, piwigo_album_id)
+
+    cursor.close()
+    conn.close()
+    pw_cursor.close()
+    pw_conn.close()
+
+def process_photos(album_id, album_name, piwigo_album_id):
+    logger.debug(f"Processing photos for album ID {album_id}, album name {album_name}, Piwigo album ID {piwigo_album_id}")
+    conn = mysql.connector.connect(**DB_CONFIG)
+    cursor = conn.cursor(dictionary=True)
+
+    cursor.execute("SELECT * FROM photos WHERE album_id=%s", (album_id,))
+    photos = cursor.fetchall()
+    logger.debug(f"Photos fetched: {photos}")
+    # Login to Piwigo
+    piwigo_login()
+    for photo in photos:
+        photo_path = None
+        if not photo['downloaded']:
+            photo_url = f"{GALLERY_BASE_URL}/{album_name}/{photo['filename']}"
+            photo_path = os.path.join(DOWNLOAD_DIR, album_name, photo['filename'])
+            os.makedirs(os.path.dirname(photo_path), exist_ok=True)
+            try:
+                download_image(photo_url, photo_path)
+                cursor.execute("UPDATE photos SET downloaded=TRUE WHERE id=%s", (photo['id'],))
+                conn.commit()
+            except Exception as e:
+                logger.error(f"Error downloading photo {photo['filename']}: {e}")
+                continue
+        else:
+            photo_path = os.path.join(DOWNLOAD_DIR, album_name, photo['filename'])
+
+        logger.debug(f"Photo: {photo['caption']} uploaded status: {photo['uploaded']}")
+        if not photo['uploaded']:
+            logger.debug(f"Photo: {photo['caption']} not uploaded, uploading")
+            logger.debug(f"Photo path: {photo_path}")
+        else:
+            logger.debug(f"Photo: {photo['caption']} already uploaded, \
+                         skipping")
+        if not photo['uploaded'] and photo_path:
+            if os.path.exists(photo_path):
+                with open(photo_path, 'rb') as f:
+                    mime_type = 'image/jpeg' if photo_path.endswith('.jpeg') or photo_path.endswith('.jpg') else 'image/png'
+                    files = {'image': (photo['filename'], f, mime_type)}
+                    if photo['description'] and photo['caption']:
+                        photo['description'] = photo['caption'] + ' - ' + photo['description']
+                    elif photo['caption']:
+                        photo['description'] = photo['caption']
+                    data = {
+                        'method': 'pwg.images.addSimple',
+                        'category': piwigo_album_id,
+                        'name': photo['caption'],
+                        'comment': photo['description'],
+					    'date_creation': photo['capturedate'].strftime('%Y-%m-%d %H:%M:%S'),  # convert to string
+					    'date_available': photo['uploaddate'].strftime('%Y-%m-%d %H:%M:%S')  # convert to string
+                    }
+                    logger.debug(f"Uploading photo: {photo['filename']} to album ID: {piwigo_album_id}")
+                    logger.debug(f"Upload data: {data}")
+                    response = session.post(PIWIGO_API_URL, data=data, files=files)
+                    logger.debug(f"Upload photo response status code: {response.status_code}")
+                    logger.debug(f"Upload photo response text: {response.text}")
+                    if response.status_code == 200:
+                        result = ElementTree.fromstring(response.content)
+                        if result.get('stat') == 'ok':
+                            cursor.execute("UPDATE photos SET uploaded=TRUE WHERE id=%s", (photo['id'],))
+                            conn.commit()
+                        else:
+                            logger.error(f"Failed to upload photo {photo['filename']}")
+                    else:
+                        logger.error(f"Failed to upload photo {photo['filename']} with status code: {response.status_code}")
+            else:
+                logger.error(f"Photo path does not exist: {photo_path}")
+
+    cursor.close()
+    conn.close()
+
+def main():
+    album_name = sys.argv[1]
+    process_album(album_name)
+
+if __name__ == "__main__":
+    main()
+
